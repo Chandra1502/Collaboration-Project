307a8afd0143dc1cd44b18b38b67cc93547abf08
Chandra1502 <chandu.pcv@gmail.com>
2017-07-04 21:54:25 +0530

    Configuration files added

---
+++ BlogAndForum/src/main/java/com/coding/blogandforum/config/AppInitializer.java
@@ -0,0 +1,25 @@
+package com.coding.blogandforum.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return new Class[] {DBConfig.class/*,WebConfig.class*/};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		return new String[]{"/"};
+	}
+
+}
---
+++ BlogAndForum/src/main/java/com/coding/blogandforum/config/DBConfig.java
@@ -0,0 +1,148 @@
+package com.coding.blogandforum.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.coding.blogandforum.DAO.BlogDAO;
+import com.coding.blogandforum.DAO.BlogDAOImpl;
+import com.coding.blogandforum.DAO.ForumDAO;
+import com.coding.blogandforum.DAO.ForumDAOImpl;
+import com.coding.blogandforum.DAO.FriendDAO;
+import com.coding.blogandforum.DAO.FriendDAOImpl;
+import com.coding.blogandforum.DAO.JobDAO;
+import com.coding.blogandforum.DAO.JobDAOImpl;
+import com.coding.blogandforum.DAO.UserDAO;
+import com.coding.blogandforum.DAO.UserDAOImpl;
+import com.coding.blogandforum.model.Blog;
+import com.coding.blogandforum.model.Forum;
+import com.coding.blogandforum.model.Friend;
+import com.coding.blogandforum.model.Job;
+import com.coding.blogandforum.model.Users;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("com")
+public class DBConfig {
+	
+	@Bean("dataSource")
+	public DataSource getDataSource()
+	{
+		//creating an Object for adding Database related properties
+				DriverManagerDataSource dataSource = new DriverManagerDataSource();
+				dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+				dataSource.setUrl("jdbc:oracle:thin:@localhost:1521/XE");
+				dataSource.setUsername("collab");
+				dataSource.setPassword("collab");
+				System.out.println("DataSource properties");
+				return dataSource;
+	}
+	
+	private Properties getHibernateProperties()
+	{
+		//creating an object for adding Hibernate related properties
+				Properties properties = new Properties();
+				properties.setProperty("hibernate.hbm2ddl.auto", "update");
+				properties.put("hibernate.show_sql", "true");
+				properties.put("hibernate.format_sql", "true");
+				properties.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+				System.out.println("hibernate properties");
+				return properties;
+	}
+	
+	@Bean("sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource)
+	{
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+		sessionBuilder.addAnnotatedClass(Users.class);
+		sessionBuilder.addAnnotatedClass(Job.class);
+		sessionBuilder.addAnnotatedClass(Blog.class);
+		sessionBuilder.addAnnotatedClass(Forum.class);
+		sessionBuilder.addAnnotatedClass(Friend.class);
+		System.out.println("Session Factory Object Created");
+		return sessionBuilder.buildSessionFactory();
+	}
+	
+	
+	@Bean("transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory)
+	{
+		System.out.println("Transaction Manager");
+		HibernateTransactionManager transactionManager=new HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
+	
+	
+	@Bean("user")
+	public Users getUsers()
+	{
+		System.out.println("user");
+		return new Users();
+	}
+	
+	@Bean("userDAO")
+	public UserDAO getUserDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("userDAO");
+		return new UserDAOImpl(sessionFactory);
+	}
+	
+	@Bean("job")
+	public Job getJob()
+	{
+		return new Job();
+	}
+	
+	@Bean("jobDAO")
+	public JobDAO getJobDAO(SessionFactory sessionFactory)
+	{
+		return new JobDAOImpl(sessionFactory);
+	}
+	
+	@Bean("blog")
+	public Blog getBlog()
+	{
+		return new Blog();
+	}
+	
+	@Bean("blogDAO")
+	public BlogDAO getBlogDAO(SessionFactory sessionFactory)
+	{
+		return new BlogDAOImpl(sessionFactory);
+	}
+	
+	@Bean("forum")
+	public Forum getForum()
+	{
+		return new Forum();
+	}
+	
+	@Bean("forumDAO")
+	public ForumDAO getForumDAO(SessionFactory sessionFactory)
+	{
+		return new ForumDAOImpl(sessionFactory);
+	}
+	
+	@Bean("friend")
+	public Friend getFriend()
+	{
+		return new Friend();
+	}
+	
+	@Bean("friendDAO")
+	public FriendDAO getFriendDAO(SessionFactory sessionFactory)
+	{
+		return new FriendDAOImpl(sessionFactory);
+	}
+}
